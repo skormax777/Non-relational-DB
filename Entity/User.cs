a0595349a59fed41ef5371008d8cb3e65c848084
Stus Maksym <58175744+skormax777@users.noreply.github.com>
2020-05-15 23:06:56 +0300

    Add files via upload

---
+++ Entity/User.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Entity
+{
+    [BsonIgnoreExtraElements]
+    public class User
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId Id { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string Login { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string FirstName { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string LastName { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string HashedPassword { get; set; }
+
+        [BsonIgnoreIfNull]
+        public List<string> Followers { get; set; }
+
+        [BsonIgnoreIfNull]
+        public List<string> Following { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string LastLogin { get; set; }
+    }
+}
