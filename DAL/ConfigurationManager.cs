9f5eaa094a4dc84a2ae67932d4ab674519ed20d8
Stus Maksym <58175744+skormax777@users.noreply.github.com>
2020-05-15 22:41:17 +0300

    Add files via upload

---
+++ DAL/ConfigurationManager.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+
+namespace DAL
+{
+    public class ConfigurationManager
+    {
+        private ConfigurationManager()
+        {
+        }
+
+        public static IMongoDatabase GetDefaultDatabase()
+        {
+            var connectionString = GetDefaultConnectionString();
+            var client = new MongoClient(connectionString);
+            return client.GetDatabase(GetDefaultDatabaseName());
+        }
+
+        private static string GetDefaultConnectionString()
+        {
+            return "mongodb://localhost:27017";
+        }
+
+        private static string GetDefaultDatabaseName()
+        {
+            return "SN";
+        }
+    }
+}
