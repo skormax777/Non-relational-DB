60661828a0d4b44cdc220fd1b7457d7009d4b847
Stus Maksym <58175744+skormax777@users.noreply.github.com>
2020-05-15 23:04:25 +0300

    Add files via upload

---
+++ BLL/LogInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace BLL
+{
+    [DataContract]
+    public class LogInfo
+    {
+        [DataMember]
+        public string Login { get; set; }
+
+        public LogInfo() {}
+        public LogInfo(string Login)
+        {
+            this.Login = Login;
+        }
+    }
+}
