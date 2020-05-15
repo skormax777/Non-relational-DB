a82d29b7f199e31d6a9e08a15ca71525815ab613
Stus Maksym <58175744+skormax777@users.noreply.github.com>
2020-05-15 23:05:53 +0300

    Add files via upload

---
+++ DAL/PostDAL.cs
@@ -0,0 +1,150 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using Entity;
+
+namespace DAL
+{
+    public class PostDAL
+    {
+        IMongoDatabase db;
+        IMongoCollection<Post> collection;
+
+        public PostDAL()
+        {
+            db = ConfigurationManager.GetDefaultDatabase();
+            collection = db.GetCollection<Post>(GetTableName());
+        }
+
+        private string GetTableName()
+        {
+            return "post";
+        }
+
+
+
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+
+
+        // Insert-------------------------------------------------------------------------
+        public void InsertPost(Post post) =>
+          collection.InsertOne(post);
+        public void InsertPosts(IEnumerable<Post> posts) =>
+            collection.InsertMany(posts);
+
+
+
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+
+
+        // Update-------------------------------------------------------------------------
+        public void UpdatePost(ObjectId postId, Post post) =>
+            collection.ReplaceOne(p => p.Id == postId, post);
+        public void UpdateByText(ObjectId postId, string newText)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var update = Builders<Post>.Update.Set("Text", newText);
+            collection.UpdateOne(filter, update);
+        }
+
+
+
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+
+
+        // Select-------------------------------------------------------------------------
+        public List<Post> SelectAllPosts(List<ObjectId> following)
+        {
+            var filter = Builders<Post>.Filter.In("OwnerId", following);
+            var posts = collection.Find(filter).ToList();
+            return posts;
+        }                  
+        public List<Post> SelectAllPosts(ObjectId OwnerId) =>
+            collection.Find(p => p.OwnerId == OwnerId).ToList();
+        public Post SelectById(ObjectId id) =>
+          collection.Find(p => p.Id == id).FirstOrDefault();
+        public List<Post> SelectNewPosts(string TimeOfLastUserLogin, List<ObjectId> following)
+        {
+            var filter = Builders<Post>.Filter.Gte("DateTime", TimeOfLastUserLogin);
+            filter = filter & Builders<Post>.Filter.In("OwnerId", following);
+            var posts = collection.Find(filter).ToList();
+            return posts;
+        }
+
+
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+
+
+        // Delete-------------------------------------------------------------------------
+        public void DeletePost(Post post) =>
+             collection.DeleteOne(p => p.Id == post.Id);
+        public void DeleteById(ObjectId postId) =>
+           collection.DeleteOne(p => p.Id == postId);
+
+
+        //--------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------
+
+
+        public void AddLike(string likerLogin, ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var update = Builders<Post>.Update.Inc("Likes", 1);
+            collection.UpdateOne(filter, update);
+
+            update = Builders<Post>.Update.Push("WhoLiked", likerLogin);
+            collection.UpdateOne(filter, update);
+        }
+        public void DismissLike(string likerLogin, ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var update = Builders<Post>.Update.Inc("Likes", -1);
+            collection.UpdateOne(filter, update);
+
+            update = Builders<Post>.Update.Pull("WhoLiked", likerLogin);
+            collection.UpdateOne(filter, update);
+        }
+
+
+        public void AddComment(Comment comment, ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var update = Builders<Post>.Update.Push("Comments", comment);
+            collection.UpdateOne(filter, update);
+        }
+        public void DeleteComment(Comment comment, ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var update = Builders<Post>.Update.Pull("Comments", comment);
+            collection.UpdateOne(filter, update);
+        }
+
+
+        public List<string> GetWhoLiked(ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var people = collection.Find(filter).Project(x => x.WhoLiked).First();
+            return people;
+
+        }
+        public int GetNumOfLikes(ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var likes = collection.Find(filter).Project(x => x.Likes).First();
+            return likes;
+        }
+
+
+        public List<Comment> GetComments(ObjectId postId)
+        {
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var comments = collection.Find(filter).Project(x => x.Comments).First();
+            return comments;
+        }
+    }
+}
---
+++ DAL/UserDAL.cs
@@ -0,0 +1,109 @@
+﻿using Entity;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DAL
+{
+    public class UserDAL
+    {
+        IMongoDatabase db;
+        IMongoCollection<User> collection;
+
+        public UserDAL()
+        {
+            db = ConfigurationManager.GetDefaultDatabase();
+            collection = db.GetCollection<User>(GetTableName());
+        }
+
+        private string GetTableName()
+        {
+            return "user";
+        }
+
+        // Insert
+        public void InsertUser(User user) =>
+           collection.InsertOne(user);
+        public void InsertUsers(IEnumerable<User> users) =>
+            collection.InsertMany(users);
+
+        // Update
+        public void UpdateUser(ObjectId id, User user) =>
+            collection.ReplaceOne(u => u.Id == id, user);
+        public void UpdateByLogin(string login, User user) =>
+            collection.ReplaceOne(u => u.Login == login, user);
+        public void UpdateByField(string login, string fieldName, string value)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var update = Builders<User>.Update.Set(fieldName, value);
+            collection.UpdateOne(filter, update);
+        }
+        public void UpdateByDateTime(string login)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var update = Builders<User>.Update.Set("LastLogin", DateTime.Now.ToString());
+            collection.UpdateOne(filter, update);
+        }
+
+        // Select
+        public List<User> SelectAllUsers() =>
+            collection.Find(u => true).ToList();
+        public User SelectById(ObjectId id) =>
+         collection.Find(u => u.Id == id).FirstOrDefault();
+        public User SelectByLogin(string login) =>
+            collection.Find(u => u.Login == login).FirstOrDefault();
+
+        // Delete
+        public void DeleteUser(User user) =>
+             collection.DeleteOne(u => u.Id == user.Id);
+        public void DeleteById(ObjectId userId) =>
+            collection.DeleteOne(u => u.Id == userId);
+        public void DeleteByNickname(string login) =>
+            collection.DeleteOne(u => u.Login == login);
+
+
+        public void AddFollowers(string login, string newFollower)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var update = Builders<User>.Update.Push("Followers", newFollower);
+            collection.UpdateOne(filter, update);
+
+        }
+        public void Unfollow(string login, string follower)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var update = Builders<User>.Update.Pull("Following", follower);
+            collection.UpdateOne(filter, update);
+
+            filter = Builders<User>.Filter.Eq("Login", follower);
+            update = Builders<User>.Update.Pull("Followers", login);
+            collection.UpdateOne(filter, update);
+        }
+        public void AddFollowing(string login, string newFollowing)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var update = Builders<User>.Update.Push("Following", newFollowing);
+            collection.UpdateOne(filter, update);
+
+        }
+        public List<string> GetFollowers(string login)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var followers = collection.Find(filter).Project(x => x.Followers).First();
+            return followers;
+        }
+        public List<string> GetFollowing(string login)
+        {
+            var filter = Builders<User>.Filter.Eq("Login", login);
+            var following = collection.Find(filter).Project(x => x.Following).First();
+            return following;
+        }
+        public ObjectId GetUserId(string login)
+        {
+            var user = collection.Find(u => u.Login == login).FirstOrDefault();
+            return user.Id;
+        }
+    }
+}
